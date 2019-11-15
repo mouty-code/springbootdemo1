7f07a8344057853428e9b34adeb860e1196e2e69
mouty-code <734421579@qq.com>
2019-11-15 20:35:45 +0800

    大佬开发更新两个方法 功能: 1.... 2...

--- src/main/java/com/offcn/springbootdemo/service/CarService.java
+++ src/main/java/com/offcn/springbootdemo/service/CarService.java
@@ -18,4 +18,10 @@ public interface CarService {
     public void updateUser(User user);
 
     public void deleteUser(Integer id);
+
+    //开发者大佬新增方法
+    public void test002();
+
+    public void test004();
+
 }
